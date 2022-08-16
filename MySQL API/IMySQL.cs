a1e8c3acac34c1a00680276a7663479755305c58
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2022-08-16 10:00:55 +0200

    a little change on the Mysql API

--- MySQL API/IMySQL.cs
+++ MySQL API/IMySQL.cs
@@ -4,15 +4,19 @@ namespace MySQL_API
 {
     public interface IMySQL
     {
-        public MySqlConnection Con { get; set; }
-        public MySqlDataReader Reader { get; set; }
-        public MySqlCommand Cmd { get; set; }
-        public string ErrorMessage { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Port { get; set; }
-        public string DBName { get; set; }
-        public string DataSource { get; set; }
-        bool LoginDB(string sql);
+        public MySqlConnection Con { get;}
+        public MySqlDataReader Reader { get;} 
+        public MySqlCommand Cmd { get;}
+        public string ErrorMessage { get;}
+        public string Username { set; }
+        public string Password { set; }
+        public string Port { set; }
+        public string DBName { set; }
+        public string DataSource { set; }
+        bool LoginDB();
+        MySqlDataReader GetReader(string sql);
+        List<string> Read(string sql);
+        void Write(string sql);
+        void SetWrite(string sql, List<string> values);
     }
 }
--- MySQL API/MysqlDB.cs
+++ MySQL API/MysqlDB.cs
@@ -5,15 +5,15 @@ namespace MySQL_API
 {
     public class MysqlDB : IMySQL
     {
-        public MySqlConnection Con { get; set; }
-        public MySqlDataReader Reader { get; set; }
-        public MySqlCommand Cmd { get; set; }
-        public string ErrorMessage { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Port { get; set; }
-        public string DBName { get; set; }
-        public string DataSource { get; set; }
+        public MySqlConnection Con { get; private set; }
+        public MySqlDataReader Reader { get; private set; }
+        public MySqlCommand Cmd { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public string Username { private get; set; }
+        public string Password { private get; set; }
+        public string Port { private get; set; }
+        public string DBName { private get; set; }
+        public string DataSource { private get; set; }
 
         public MysqlDB(string username, string password, string port, string dBName, string dataSource)
         {
@@ -24,17 +24,15 @@ namespace MySQL_API
             DataSource = dataSource;
         }
 
-        public bool LoginDB(string sql)
+        #region Login into Database
+        public bool LoginDB()
         {
             try
             {
-                string connection = (@$"Data Source = {DataSource}; Port = {Port} ; Initial Catalog = {DBName}; User Id = {Username}; Password = {Password};");
+                string connection = (@$"Data Source = {DataSource}; Port = {Port} ; 
+                                        Initial Catalog = {DBName}; User Id = {Username}; Password = {Password};");
                 Con = new MySqlConnection(connection);
                 Con.Open();
-                Cmd = Con.CreateCommand();
-                Cmd.CommandType = CommandType.Text;
-                Cmd.CommandText = sql;
-                Cmd.ExecuteNonQuery();
                 return true;
             }
             catch (Exception e)
@@ -45,5 +43,102 @@ namespace MySQL_API
                 return false;
             }
         }
+        #endregion
+
+        #region Read from Database with String List Return
+
+        // Read all Columns from Database and saved it in string as List, later can be split by ;
+        public List<string> Read(string sql)
+        {
+            List<string> list = new();
+
+            if (LoginDB())
+            {
+                Cmd = Con.CreateCommand();
+                Cmd.CommandType = CommandType.Text;
+                Cmd.CommandText = sql;
+                Reader = Cmd.ExecuteReader();
+
+                if (Reader.HasRows)
+                {
+                    while (Reader.Read())
+                    {
+                        string column = string.Empty;
+                        for (int i = 0; i < Reader.FieldCount; i++)
+                        {
+                            column += Reader.GetString(i);
+                            if (i == Reader.FieldCount - 1)
+                                break;
+                            column += ";";
+                        }
+                        list.Add(column);
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        #endregion
+
+        #region Read from Database with MysqlDataReader return
+
+        public MySqlDataReader GetReader(string sql)
+        {
+            if (LoginDB())
+            {
+                Cmd = Con.CreateCommand();
+                Cmd.CommandType = CommandType.Text;
+                Cmd.CommandText = sql;
+                return Reader = Cmd.ExecuteReader();
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Write Data into DB
+
+        // need sql Statement insert into Table values (data, data, data);
+        public void Write(string sql)
+        {
+            if (LoginDB())
+            {
+                Cmd = Con.CreateCommand();
+                Cmd.CommandType = CommandType.Text;
+                Cmd.CommandText = sql;
+                Cmd.ExecuteNonQuery();
+                Con.Close();
+            }
+        }
+
+        #endregion
+
+        #region Send Data to Database table for Tablename and Values for (data, "data", data);
+        public void SetWrite(string table, List<string> values)
+        {
+            string query = $"insert into {table} values ";
+            for (int i = 0; i < values.Count; i++)
+            {
+                query += values[i];
+
+                if (i == values.Count - 1)
+                {
+                    query += ";";
+                    break;
+                }
+                query += ",";
+            }
+
+            if (LoginDB())
+            {
+                Cmd = Con.CreateCommand();
+                Cmd.CommandType = CommandType.Text;
+                Cmd.CommandText = query;
+                Cmd.ExecuteNonQuery();
+                Con.Close();
+            }
+        }
+        #endregion
     }
 }
